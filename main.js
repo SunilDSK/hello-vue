b6f82f8952528631ddf2b2b643514fa0b39f2363
Sunil <sunillangtad54@gmail.com>
2020-03-12 16:32:45 +0530

    Hello vue

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-const app = new Vue({
-    el: "#app",
-    data: {
-        name: 'Sunil'
-    },
-    template: `
-        <div>
-            <h1>Hi Vue</h1>
-            <h1>This is from Vue.js {{name}}</h1>
-        </div>
-    `
-}); 
+new Vue({
+  el: "#app",
+  data: {
+    title: "Hello Vue.js"
+  }
+});
