03a5e0724a150e2c4d8a85ecc5b770bd34e30b34
Sunil <sunillangtad54@gmail.com>
2020-03-12 18:20:54 +0530

    v-if with conditions in html

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ var hours = new Date().getHours();
 new Vue({
   el: "#app",
   data: {
-      isMorning: hours < 12,
-      isAfternoon: hours >= 12 && hours <= 18,
-      isEvening: hours >18
+      hours: new Date().getHours()
   }
 });
