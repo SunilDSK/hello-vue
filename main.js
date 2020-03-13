507f4c8640c92867780b33aec798bad5ac947d3d
Sunil <sunillangtad54@gmail.com>
2020-03-13 13:30:15 +0530

    uses v-bind

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-new Vue({
+var vue = new Vue({
   el: "#app",
   data: {
-    averageRent: {
-      london: 1650,
-      paris: 1730,
-      NYC: 3680
-    }
+    buttonType: "submit",
+    buttonDisabled: false
   }
 });
