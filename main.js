e2c72392ba6750bc50032027374fb957d42b5e6f
Sunil <sunillangtad54@gmail.com>
2020-03-13 16:50:39 +0530

    Array manipulation

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 var vue = new Vue({
   el: "#app",
   data: {
-    formData: {
-      username: '{someuser}'
-    }
+    dogs: ['D1','D2','D3']
   }
 });
 
-Vue.set(vue.formData, 'name', '{Some User}');
+// Use splice to change array content
+vue.dogs.splice(2, 1, 'Bob');
+
+
+// Use set to change array content
+Vue.set(vue.dogs, 1, 'Jam');
+vue.$set(vue.dogs, 0, 'Jak');
+
+vue.dogs.splice(2);
